93c100ecb503dd793ad7b9b409099e947c89c616
bghayadh <bilmar_gh@yahoo.com>
2021-05-28 17:51:53 -0400

    Initial commit

---
+++ app/src/main/java/com/example/aliatsimactivation/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.aliatsimactivation;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_CODE = 100;
+
+    //define buttons
+    private Button BtnMobCharge, BtnResCharge,BtnExit,BtnSIMReg;
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_main);
+
+        //initialize objects on Load
+        BtnSIMReg=findViewById(R.id.Btnsimreg);
+        BtnMobCharge=findViewById(R.id.Btnmobcharge);
+        BtnResCharge=findViewById(R.id.Btnrescharge);
+        BtnExit=findViewById(R.id.BtnExit);
+
+        // check if we have permission to get our location in manifest xml file
+        try {
+            if (ContextCompat.checkSelfPermission (getApplicationContext ( ), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions (this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            }
+        } catch (Exception e) {
+            e.printStackTrace ( );
+        }
+
+        // click to move to Sim Registration List
+        BtnSIMReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Show message to know where are we transferred
+                Toast.makeText(MainActivity.this,  "Welcome to Sim Registration page",Toast.LENGTH_SHORT).show();
+                //Intent intent = new Intent(MainActivity.this, SIMRegistrationListView.class);
+                // startActivity(intent);
+            }
+
+        });
+
+
+
+        // click to move to Mobile Charge List
+        BtnMobCharge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Show message to know where are we transferred
+                Toast.makeText(MainActivity.this,  "Welcome to Mobile Charge page",Toast.LENGTH_SHORT).show();
+                // Intent intent =new Intent(MainActivity.this,MobileChargeListViewActivity.class);
+                // startActivity(intent);
+            }
+        });
+
+        //click to move to Reseller Charge List
+        BtnResCharge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Show message to know where are we transferred
+                Toast.makeText(MainActivity.this,  "Welcome to Reseller Charge page",Toast.LENGTH_SHORT).show();
+                // Intent intent =new Intent(MainActivity.this,ResellerChargeListViewActivity.class);
+                // startActivity(intent);
+            }
+        });
+
+
+        // exit button
+        BtnExit.setOnClickListener (new View.OnClickListener ( ) {
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/aliatsimactivation/OraDB.java
@@ -0,0 +1,31 @@
+package com.example.aliatsimactivation;
+
+public class OraDB {
+    private String oraurl;
+    private String orausername;
+    private String orapwd;
+
+
+
+    public OraDB() {
+        oraurl = "jdbc:oracle:thin:@192.168.2.191:1521:ALM";
+        orausername = "alm";
+        orapwd = "alm";
+    }
+
+
+    public String getoraurl() {
+        return oraurl;
+    }
+
+    public String getorausername() {
+        return orausername;
+    }
+
+    public String getorapwd() {
+        return orapwd;
+    }
+
+
+
+}
