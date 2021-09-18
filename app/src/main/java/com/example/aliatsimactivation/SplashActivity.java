81fb656362cf462364301a150eaeefdb0ebac9b0
bghayadh <bilmar_gh@yahoo.com>
2021-09-18 18:40:39 -0400

    update Agent

--- app/src/main/java/com/example/aliatsimactivation/SplashActivity.java
+++ app/src/main/java/com/example/aliatsimactivation/SplashActivity.java
@@ -16,7 +16,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i=new Intent(getApplicationContext(),UserLoginActivity.class);
+                Intent i=new Intent(getApplicationContext(),AgentLogin.class);
                 startActivity(i);
                 finish();
             }
